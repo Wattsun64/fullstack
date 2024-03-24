365ce16a2dd5de0d81ce2b382fb4422b7f95d7d0
Edward Cochran <ecochra@gmail.com>
2024-03-23 22:52:51 -0400

    Created components dir

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header"
+import Content from "./Content"
+
+const Course = ({ course }) => {
+  const total = course.parts.reduce((acc, part) => acc + part.exercises, 0)
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <p><strong>total of {total} exercises</strong></p>
+    </>
+  )
+}
+
+export default Course
