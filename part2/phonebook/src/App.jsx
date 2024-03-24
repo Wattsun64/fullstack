d6d544dd42c38bde66100decad73862a7a34332f
Edward Cochran <ecochra@gmail.com>
2024-03-24 00:00:22 -0400

    Built out application

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Phonebook from './components/Phonebook'
+import Search from './components/Search'
+import Form from './components/PersonForm'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [newName, setNewName] = useState('')
+  const [search, setSearch] = useState('')
+
+  const handleNewPersonInput = (evt) => {
+    setNewName(evt.target.value)
+  }
+
+  const handleNewPhoneNumberInput = (evt) => {
+    setNewPhoneNumber(evt.target.value)
+  }
+
+  const handleSearch = (evt) => {
+    setSearch(evt.target.value)
+  }
+
+  const nameExistsOrNot = (name) => {
+    return persons.filter(person => person.name.toLocaleLowerCase() == name.toLocaleLowerCase()).length > 0
+  }
+
+  const handlePersonSubmit = (evt) => {
+    evt.preventDefault();
+    if (!nameExistsOrNot(newName))
+      setPersons(persons.concat({name: newName, number: newPhoneNumber}))
+    else 
+      window.alert(`${newName} is already added to phonebook`)
+    setNewName('')
+    setNewPhoneNumber('')
+  }
+
+  const filteredPersons = persons.filter(person => person.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <Search value={search} onInputHandler={handleSearch} />
+      <h3>Add a new</h3>
+      <Form
+        name={newName} nameHandler={handleNewPersonInput}
+        number={newPhoneNumber} numberHandler={handleNewPhoneNumberInput}
+        submitHandler={handlePersonSubmit} />
+      <h3>Numbers</h3>
+      <Phonebook people={filteredPersons} />
+    </div>
   )
 }
 
